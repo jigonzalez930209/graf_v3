3d00f1715d60e97a4b4dae69df2fd41c31108385
Jose Ivan <jigonzalez930209@gmail.com>
2025-02-28 07:47:47 -0300

    feat: Add resize panels

--- src/renderer/src/components/graf.tsx
+++ src/renderer/src/components/graf.tsx
@@ -11,42 +11,60 @@ import { PlotParams } from 'react-plotly.js'
 import { IProcessFile } from '@shared/models/files'
 
 import usePlotlyOptions from '../hooks/usePlotlyOptions'
+import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from './ui/resizable'
 import Loader from './loader'
 
 const Graf = () => {
   const { graftState } = React.useContext(GrafContext)
   const { isLoading } = useLoading()
+  const [width, setWidth] = React.useState(0)
 
   const { data, layout, config } = usePlotlyOptions()
 
   return (
-    <>
+    <div>
       {isLoading && <Loader />}
       <div className="flex max-h-full max-w-full">
-        <Drawer />
-        {graftState?.fileType === 'csv' ? (
-          <DragDrop
-            PlotlyChart={
+        <ResizablePanelGroup direction="horizontal" className="w-full h-full">
+          <ResizablePanel defaultSize={25}>
+            <Drawer />
+          </ResizablePanel>
+          <ResizableHandle withHandle />
+          <ResizablePanel
+            defaultSize={75}
+            onResize={(i) => {
+              setWidth(i)
+            }}
+          >
+            {graftState?.fileType === 'csv' ? (
+              <DragDrop
+                PlotlyChart={
+                  <PlotlyChart
+                    layout={layout as PlotParams['layout']}
+                    config={config}
+                    data={data}
+                    fileType={graftState.fileType}
+                    width={width}
+                    exportFileName={
+                      graftState.files.find((file) => file.selected)?.name ?? undefined
+                    }
+                  />
+                }
+              />
+            ) : (
               <PlotlyChart
                 layout={layout as PlotParams['layout']}
                 config={config}
                 data={data}
-                fileType={graftState.fileType}
-                exportFileName={graftState.files.find((file) => file.selected)?.name ?? undefined}
+                width={width}
+                fileType={graftState.fileType as IProcessFile['type']}
+                exportFileName={graftState.files.find((file) => file.selected)?.name}
               />
-            }
-          />
-        ) : (
-          <PlotlyChart
-            layout={layout as PlotParams['layout']}
-            config={config}
-            data={data}
-            fileType={graftState.fileType as IProcessFile['type']}
-            exportFileName={graftState.files.find((file) => file.selected)?.name}
-          />
-        )}
+            )}
+          </ResizablePanel>
+        </ResizablePanelGroup>
       </div>
-    </>
+    </div>
   )
 }
 export default Graf
